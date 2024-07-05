226f24af9be9c627a43f3ef9d64b54c0b158e337
byGranizo <migueljg1997@gmail.com>
2024-07-05 16:09:16 +0200

    fix: deploy

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/tfm-computational-thinking-dashboard/'>
       <App />
     </BrowserRouter>
   </React.StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/tfm-computational-thinking-dashboard/',
   plugins: [react()],
   resolve: {
     alias: {
       '@': '/src',
     },
   },
-  base: '/tfm-computational-thinking-dashboard/',
 })
